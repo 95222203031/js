ad9a8b64947991227c638956eca97d3dda6fa16d
95222203031 <kkasthurimurugaiah@gmail.com>
2024-08-30 12:05:03 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,76 @@
+const pi = 3.141459;
+console.log(pi);
+var name="kasthuri"
+console.log("name",name);
+let h1 ="hi"
+console.log("h1:",h1);
+
+let age=23;
+const name1="kasthu";
+var Isstudent =true;  
+
+let person ={
+    firstname:"kasthu",
+    lastname:"gayu",
+    age:23,
+    Isstudent:true
+};
+
+console.log("name1,age,Isstudent");
+console.log(person);
+
+//rectangle//
+let b=45;
+let h=50;
+let d =b*h;
+console.log("area of rectangle =",d);
+//rectangle//
+
+//circle//
+let a=pi;
+let f=5**2;
+let v=a*f;
+ console.log("Area of circle= ",v);
+//circle//
+
+//square//
+let c=5**2;
+console.log("area of square =",c);
+//square//
+
+//triangle//
+let z=0.5;
+let l=23;
+let r=45;
+let o=z*l*r;
+console.log("area of triangle =",o);
+//triangle//
+
+//cylinder//
+let a1=2*3.14*5;
+let r2=5;
+let h2=3;
+let x=a1*(r2+h2);
+console.log("area of cylinder= ",x);
+//cylinder//
+//semi circle//
+let a2=pi;
+let r3=2**2;
+let s2=0.5;
+let x2=a2*r3*0.5;
+console.log("area of semi circle =",x2);
+//semi circle//
+
+
+//student mark&average//
+var name2 ="kalai" ;
+let tamil=100;
+let english=99;
+let maths=78;
+let science=89;
+let social=99;
+let total =tamil+english+maths+science+social;
+let average=total/5;
+console.log("total of the student=",total);
+console.log("average of the student=",average ,"%");
+//student mark&average//
